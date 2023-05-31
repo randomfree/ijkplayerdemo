9cf6cadb729d8a393a29916e6c48a475ee46366f
ly <ly@mt-001deMacBook-Pro.local>
2023-05-31 19:00:33 +0800

    first

---
+++ app/src/main/java/com/ly/demo2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ly.demo2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.SurfaceTexture;
+import android.os.Bundle;
+import android.view.TextureView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
